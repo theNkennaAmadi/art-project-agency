0ee45bc251cb5905db8b4a5716b1ea309e06bc92
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2024-09-05 13:24:51 +0100

    remove animations

--- home.js
+++ home.js
@@ -4,43 +4,9 @@ export class Home {
 	constructor(container) {
 		this.container = container
 		this.init()
-
-		this.heroSection()
 	}
 
 	init() {
 		// console.log('Home')
 	}
-
-	heroSection() {
-		const centerX = -1.5
-		const centerY = -2.5
-
-		gsap
-			.timeline()
-			.to('.home-hero-svg', {
-				scale: 150,
-				xPercent: -centerX * 150,
-				yPercent: -centerY * 150,
-				ease: 'expo.inOut',
-				delay: 2,
-				duration: 1.75,
-			})
-			.to(
-				'.home-hero-svg',
-				{
-					autoAlpha: 0,
-				},
-				'-=.3'
-			)
-			.to(
-				'.home-hero-btn',
-				{
-					autoAlpha: 1,
-				},
-				'-=.5'
-			)
-	}
 }
-
-new Home()
